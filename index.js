e5f299fd7a9146ec4a359a0a844bcc56b459fa8b
Smoore01 <ibabatunde03gmail.com>
2026-01-13 09:02:04 -0600

    Allow localhost frontend in CORS

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ dotenv.config();
 
 const app = express();
 
+
 app.use(
   cors({
-    origin: true, 
+    origin: "http://localhost:5173", 
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
-)
-
-app.use(cors());
+);
 
 
 app.use(express.json());
@@ -24,18 +25,17 @@ app.use(express.json());
 
 connectDB(process.env.MONGO_URI);
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/habits", habitRoutes);
 
 
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
